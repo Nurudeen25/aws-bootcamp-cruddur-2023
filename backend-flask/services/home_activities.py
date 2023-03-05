d786e251077db0c6e1f41497f492a67e3f103598
Nurudeen25 <olaniyi_nuru@aol.com>
2023-03-05 22:55:32 +0000

    implement backend check for token

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,13 +6,14 @@ tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   # def run(logger):  this is to turn on cloudwatch logs
-  def run():
+  def run(cognito_user_id=None):
     # logger.info("HomeActivities")
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
       span.set_attribute("UserId", now.isoformat())
+
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
@@ -52,5 +53,18 @@ class HomeActivities:
       'replies': []
     }
     ]
+
+    if cognito_user_id != None:
+      extra_crud = {
+        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+        'handle':  'Lore',
+        'message': 'The nice guy!',
+        'created_at': (now - timedelta(hours=1)).isoformat(),
+        'expires_at': (now + timedelta(hours=12)).isoformat(),
+        'likes': 1655,
+        'replies': []
+      }
+      results.insert(0,extra_crud)
+
     span.set_attribute("app.result_length", len(results))
     return results
