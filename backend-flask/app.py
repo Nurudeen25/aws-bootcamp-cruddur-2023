9ee9d5809493b6900d450ec25db7f761bcf3589f
Nurudeen25 <olaniyi_nuru@aol.com>
2023-03-12 22:31:04 +0000

    add db.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -167,7 +167,7 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
-@xray_recorder.capture('activities_home')
+# @xray_recorder.capture('activities_home')
 def data_home():
   # data = HomeActivities.run(logger= LOGGER) this is for cloudwatch logs
   access_token = extract_access_token(request.headers)
