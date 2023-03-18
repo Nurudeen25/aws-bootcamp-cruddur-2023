f300b4691444f80014e4abec133f4eed12f19f7d
Nurudeen25 <olaniyi_nuru@aol.com>
2023-03-18 13:54:08 +0000

    Revert "add color into print SQL"
    This reverts commit 28b84180e1cdd75f17a40fcef371dfdb061b447a.

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -2,14 +2,13 @@ from psycopg_pool import ConnectionPool
 import os
 import re
 import sys
-from flask import current_app as app 
 
 class Db:
   def __init__(self):
     self.init_pool()
 
-  def template(self,name):
-    template_path = os.path.join(app.root_path,'db','sql',name+'.sql')
+  def template(name):
+    template_path = os.path.join(app.instance_path,'db','sql',name+'.sql')
     with open(template_path, 'r') as f:
       template_content = f .read()
     return template_content
@@ -19,25 +18,23 @@ class Db:
     self.pool = ConnectionPool(connection_url)
   # when we want to commot data such as an insert
   # be sure to check for RETURNING in all uppercases
-  def print_sql(self,title,sql):
-    cyan = '\033[96m'
-    no_color = '\033[0m'
-    print("\n")
-    print(f'{cyan}SQL STATEMENT-[{title}]------{no_color}')
-    print(sql + "\n")
   def query_commit(self,sql,params):
-    self.print_sql('commit with returning',sql)
+    print("SQL STATEMENT-[commit with returning]------------")
+    print(sql + "\n")
+
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
+    
+
     try:
-      with self.pool.connection() as conn:
-        cur  = conn.cursor()
-        cur.execute(sql,params)
-        if is_returning_id:
-          returning_id = cur.fetchone()[0]
-        conn.commit()
-        if is_returning_id:
-          return returning_id
+      conn = self.pool.connection()
+      cur  = conn.cursor()
+      cur.execute(sql,params)
+      if is_returning_id:
+        returning_id = cur.fetchone()[0]
+      conn.commit()
+      if is_returning_id:
+        return returning_id
     except Exception as err:
       self.print_sql_err(err)
   # when we want to return a json object
--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -54,7 +54,6 @@ class CreateActivity:
         'expires_at': (now + ttl_offset).isoformat()
       }
     return model
-    
   def create_activity(handle, message, expires_at):
     sql = db.template('create_activity')
     uuid = db.query_commit(sql,{
