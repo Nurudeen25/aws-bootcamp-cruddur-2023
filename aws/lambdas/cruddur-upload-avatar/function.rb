2c852fef516a09dc7106759919f29261b51cea2d
Nurudeen25 <olaniyi_nuru@aol.com>
2023-04-23 22:57:09 +0000

    make sure that can upload avatar

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -1,23 +1,49 @@
 require 'aws-sdk-s3'
 require 'json'
+require 'jwt'
 
 def handler(event:,context:)
   puts event
-  s3 = Aws::S3::Resource.new
-  bucket_name = ENV['UPLOADS_BUCKET_NAME']
-  object_key = 'mock.jpg'
+  # return cors headers for preflight check
+  if event['routeKey'] == "OPTIONS /{proxy+}"
+    puts ({step: 'preflight', message: 'preflight CORS check'}.to_json)
+    { 
+      headers {
+        "Access-Control-Allow-Headers": "*, Authorization",
+        "Access-Control-Allow-Origin": "https://3000-nurudeen25-awsbootcampc-7qqp9qg1r8w.ws-us95.gitpod.io",
+        "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+      },
+      statusCode: 200     
+    }
+  else
+    token = events['headers']['authorization'].split('')[1]
+    puts ({step: 'presignedurl', access_token: token}.to_json)
 
-  obj = s3.bucket(bucket_name).object(object_key)
-  url = obj.presigned_url(:put, expires_in: 60 * 5)
-  url # this is the data that will be returned
-  body = {url: url}.to_json
-  { 
-    headers {
-      "Access-Control-Allow-Headers": "*, Authorization",
-      "Access-Control-Allow-Origin": "https://3000-nurudeen25-awsbootcampc-jgcbdx4gfti.ws-us95.gitpod.io/",
-      "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
-    },
-    statusCode: 200, 
-    body: body
-   }
+    body_hash = JSON.parse(event["body"])
+    extension = body_hash["extension"]
+
+    decoded_token = JWT.decode token, nil, false
+    puts "decoded token"
+    cognito_user_uuid = decoded_token[0]['sub']
+
+    s3 = Aws::S3::Resource.new
+    bucket_name = ENV['UPLOADS_BUCKET_NAME']
+    object_key = "#{cognito_user_uuid}.#{extension}"
+
+    puts({object_key: object_key}.to_json)
+  
+    obj = s3.bucket(bucket_name).object(object_key)
+    url = obj.presigned_url(:put, expires_in: 60 * 5)
+    url # this is the data that will be returned
+    body = {url: url}.to_json
+    { 
+      headers {
+        "Access-Control-Allow-Headers": "*, Authorization",
+        "Access-Control-Allow-Origin": "https://3000-nurudeen25-awsbootcampc-7qqp9qg1r8w.ws-us95.gitpod.io",
+        "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+      },
+      statusCode: 200,
+      body: body
+    }
+  end
 end
