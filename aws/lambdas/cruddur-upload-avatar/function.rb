14134f41cfb8a4f6ba220a6d9290475a4a1c9ac1
Nurudeen25 <olaniyi_nuru@aol.com>
2023-05-01 20:40:58 +0000

    Render avatar from cloudfrnt implemented successful

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -10,7 +10,7 @@ def handler(event:, context:)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://3000-nurudeen25-awsbootcampc-josu1b5ncaq.ws-us96b.gitpod.io",
+        "Access-Control-Allow-Origin": "https://3000-nurudeen25-awsbootcampc-8g7ip1iiww9.ws-us96b.gitpod.io",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200
@@ -38,7 +38,7 @@ def handler(event:, context:)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://3000-nurudeen25-awsbootcampc-josu1b5ncaq.ws-us96b.gitpod.io",
+        "Access-Control-Allow-Origin": "https://3000-nurudeen25-awsbootcampc-8g7ip1iiww9.ws-us96b.gitpod.io",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200, 
--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -1,8 +1,7 @@
 import './ProfileAvatar.css';
 
 export default function ProfileAvatar(props) {   
-  // const backgroundImage = 'url("https://assets.harvestdam.com/avatars/${props.id}.jpg")';
-  const backgroundImage = 'url("https://assets.harvestdam.com/avatars/data.jpg")';
+  const backgroundImage = `url("https://assets.harvestdam.com/avatars/${props.id}.jpg")`;
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
